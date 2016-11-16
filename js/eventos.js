1469591f920351f4702fcda2ae14399cf7030126
yanirahauchbaum <yanira.hauchbaum@gmail.com>
2016-11-16 12:02:13 -0700

    JQuery Continuación

--- js/eventos.js
+++ js/eventos.js
@@ -3,7 +3,15 @@ var inicio = function() //Main
 {
 	var dameclic = function()
 	{
-		alert("Le di clic a un botón");
+		$.ajax({
+  		url: 'https://randomuser.me/api/',
+  		dataType: 'json',
+ 		success: function(data) {
+   		$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
+   		$("#imgFoto").attr("src",data.results[0].picture.large);
+   		console.log(data.results[0].name.first+" "+data.results[0].name.last);
+    	}
+		});
 	}
 	$("#dameClic").on("click",dameclic);
 }
